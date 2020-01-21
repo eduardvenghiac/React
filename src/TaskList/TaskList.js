686c1960bafcfed5aab241cab9797fe059dda121
Eduard Venghiac <eduard.venghiac@WS-IS-U2-1472.local>
2020-01-21 17:31:26 +0200

    renaming

--- src/TaskList/TaskList.js
+++ src/TaskList/TaskList.js
@@ -7,30 +7,30 @@ class TaskList extends Component{
 
     state = {
         taskNames:[],
-        text:''
+        taskName:''
     }
 
     removeTask = (i) => {
         let arr = this.state.taskNames;
         arr.splice(i,1);
-        this.setState({descriptions:arr})
+        this.setState({taskNames:arr})
     }
 
     updateTask = (newDescription,i) => {
         let arr = this.state.taskNames;
         arr[i] = newDescription;
-        this.setState({descriptions:arr})
+        this.setState({taskNames:arr})
     }
 
     handleChange = (event) => {
-        this.setState({text:event.target.value});
+        this.setState({taskName:event.target.value});
     }
 
     handleSubmit = () => {
-        if(this.state.text.length==0){
+        if(this.state.taskName.length==0){
             return;
         }
-        this.setState({descriptions:this.state.taskNames.concat(this.state.text),text:''});
+        this.setState({taskNames:this.state.taskNames.concat(this.state.taskName),taskName:''});
     }
 
     render(){
@@ -39,17 +39,17 @@ class TaskList extends Component{
                 <div className="taskList">
                     <p>Tasks:{this.state.taskNames.length}</p>
                     {
-                        this.state.taskNames.map((text,i) =>
+                        this.state.taskNames.map((taskName,i) =>
                         {
                             return (
-                            <Task key={i} index={i} updateTaskDescription={this.updateTask} removeTaskFromList={this.removeTask}>{text}</Task>
+                            <Task key={i} index={i} updateTaskDescription={this.updateTask} removeTaskFromList={this.removeTask}>{taskName}</Task>
                             );
                         })
                     }
                 </div>
                 <p>Add new task here:</p>
                 <div className="newTask">
-                    <input type="text" onChange={this.handleChange} value={this.state.text}/>
+                    <input type="text" onChange={this.handleChange} value={this.state.taskName}/>
                     <Button type="handleSubmit" name="Add task" handleSubmitTask={this.handleSubmit}/>
                 </div>
             </main>
